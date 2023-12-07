807898465f70b8d32a1bb4c9d16168640aa76b63
Gustavo <gugu.paiva892@gmail.com>
2023-12-07 10:50:40 -0300

    api_finalizada

---
+++ src/main/java/br/dronav/cefet/ApiDronavApplication.java
@@ -0,0 +1,13 @@
+package br.dronav.cefet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDronavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDronavApplication.class, args);
+	}
+
+}
